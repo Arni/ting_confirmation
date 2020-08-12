b9cce0662cd342988b1b99becae873e236b655a8
DQ26525 <DQ26525@LT8085.Randers.dk>
2020-08-12 12:25:26 +0200

    Initial commit

---
+++ templates/confirmation-object.tpl.php
@@ -0,0 +1,31 @@
+
+<?php
+/**
+ * @file
+ * Modified version of the default theme implementation for displaying a single
+ * search result. The h3 title have been removed.
+ *
+ *
+ * @see template_preprocess()
+ * @see template_preprocess_search_result()
+ * @see template_process()
+ */
+?>
+<div class="conformation-object">
+    <div class="conformation-object-cover">
+        <?php print $cover; ?>
+    </div>
+    <div class="conformation-object-message">
+        <h2>Tak for din reservering</h2>
+        <?php print $message; ?>
+    </div>
+</div>
+<div class="conformation-object-inspiration">
+    <div class="more-link"><a href="#recommender">Find lignende bøger</a></div>
+    <div class="more-link"><a href="/inspiration/inspiration">Bliv inspireret</a></div>
+    <div class="more-link"><a href="https://nytpaahylderne.dk/ranbib">Nyt på hylderne</a>
+</div>
+
+
+
+
